2cf378980abecad7d991e02ea0a319ee83fad15d
Jorge Stefanov <georgi.stefanov@flyrlabs.com>
2024-05-14 11:22:25 +0300

    adjusted time format for lll

--- etc/profile.d/z-aliases.sh
+++ etc/profile.d/z-aliases.sh
@@ -15,7 +15,7 @@ alias less='less -R'
 
 if [[ "$OSTYPE" != darwin* ]]; then
     # long format with ISO dates
-    alias lll='ls --color=auto -Al "--time-style=+%Y-%m-%d %H:%M:%S %4Z"'
+    alias lll='ls --color=auto -Al "--time-style=+%Y-%m-%d %H:%M:%S GMT%z"'
 
     # color ip route
     if ip -V | grep '^ip utility, iproute2-.*, libbpf' &>/dev/null; then
@@ -28,5 +28,5 @@ if [[ "$OSTYPE" != darwin* ]]; then
 else
     # MacOS aliases
     alias ll='ls --color=auto -l'
-    alias lll='ls --color=auto -Al -D "%Y-%m-%d %H:%M:%S %Z"'
+    alias lll='ls --color=auto -Al -D "%Y-%m-%d %H:%M:%S GMT%z"'
 fi
