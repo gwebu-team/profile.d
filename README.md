45d926a3a91256d608638ebcd71656e41f3a922c
Doncho N. Gunchev <dgunchev@gmail.com>
2024-05-30 03:57:04 +0300

    New shebang for changelog.sh, post-commit hook in README.md.

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 This is a collection of /etc/profile.d files used by the gwebu team.
 Unless explicitly stated in the file itself, the license is [The Unlicense](https://choosealicense.com/licenses/unlicense/).
+
+## Hooks
+
+Create the following hook in your local repository to update the RPM changelog for you:
+
+
+```bash
+echo '#!/usr/bin/env bash
+
+set -eu
+
+./changelog.sh' > .git/hooks/post-commit
+chmod a+x .git/hooks/post-commit
+```
--- changelog.sh
+++ changelog.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
+shopt -s nullglob dotglob
 
 ver=$(git describe --dirty --long --match='v[0-9]*.[0-9]*' | cut -c 2- | cut -d - -f 1,2,4)
 rpm_ver="${ver%%-*}"
@@ -14,3 +17,7 @@ awk -v change="${change}" '/^%changelog/ {print; print change; next} 1' gwebu-pr
 
 sed -i "s/Version: .*/Version: ${rpm_ver}/" gwebu-profile.spec.in
 sed -i "s/Release: .*/Release: ${rpm_numeric_rev}/" gwebu-profile.spec.in
+
+git add gwebu-profile.spec.in
+
+echo "gwebu-profile.spec.in changelog updated"
